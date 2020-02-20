1f5780b95b5428a33e6bc981d74df53ba7c1fc08
anselbrandt <ansel@anselbrandt.com>
2020-02-20 02:39:43 -0500

    Add Dropzone component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
-import styles from "./App.module.css";
+import React from 'react';
+import styles from './App.module.css';
+import Dropzone from './Dropzone';
 
 function App() {
-  return <div className={styles.app}>Hello.</div>;
+  return (
+    <div className={styles.app}>
+      <Dropzone />
+    </div>
+  );
 }
 
 export default App;
