50f0ada07a95c9a1d1c836746d1f193c2d0766fb
anselbrandt <ansel@anselbrandt.com>
2020-02-21 14:18:11 -0500

    Move onDrop handler to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './App.module.css';
 import Dropzone from './Dropzone';
 
 function App() {
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach(file => {
+      console.log(file.name);
+      const reader = new FileReader();
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsText(file);
+    });
+  }, []);
+
   return (
     <div className={styles.app}>
-      <Dropzone />
+      <Dropzone onDrop={onDrop} />
     </div>
   );
 }
