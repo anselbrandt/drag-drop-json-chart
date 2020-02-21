ae3c1a3c5e737b70719e87cbe8f1cdea4c520695
anselbrandt <ansel@anselbrandt.com>
2020-02-21 13:48:57 -0500

    Add styling to Dropzone

--- src/Dropzone.js
+++ src/Dropzone.js
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import styles from './App.module.css';
 
 export default function Dropzone() {
   const onDrop = useCallback(acceptedFiles => {
@@ -19,13 +20,26 @@ export default function Dropzone() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+    <div className={styles.dropContainer}>
+      <svg
+        className="box__icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="43"
+        viewBox="0 0 50 43"
+        stroke="lightslategrey"
+        fill="lightslategrey"
+      >
+        <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"></path>
+      </svg>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drop JSON file here.</p>
+        )}
+      </div>
     </div>
   );
 }
